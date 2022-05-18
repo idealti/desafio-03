2908248b354362464a82af7ab4ca1876005e4913
Carlos Eduardo <cadu4duarte@gmail.com>
2022-05-18 19:32:37 -0300

    style: add global style

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { router } from "./routers"
+import "./global.css"
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
