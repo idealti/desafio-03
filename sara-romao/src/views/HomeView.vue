995d9a9b2d9ccd607ceee7b66387af9c4399df2d
Sara Romão <sara-romao@live.com>
2022-05-19 11:05:40 -0300

    fetch na api

--- sara-romao/src/views/HomeView.vue
+++ sara-romao/src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>home</h1>
+
+    <main class="listProducts">
+      
+      <div v-if="listProducts !== null">
+        <div v-for="product in listProducts" :key="product.id">
+          <p>{{ product.title }}</p>
+        </div>
+      </div>
+
+      <div v-else>
+        <p>Carregando...</p>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'HomeView',
-  
-}
+  name: "HomeView",
+
+  data() {
+    return {
+      listProducts: null,
+      id: 1
+    };
+  },
+
+  methods: {
+    async fetchApi() {
+      const res = await fetch(`https://fakestoreapi.com/products`);
+      const data = await res.json();
+      this.listProducts = data;
+      
+    },
+  },
+
+  mounted() {
+    this.fetchApi();
+  },
+};
 </script>
 
 <style scoped>
-
+main{
+  max-width: 1200px;
+  margin: 0 auto;
+  border: 1px solid red;
+}
 </style>
