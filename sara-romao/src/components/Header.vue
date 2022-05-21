abcf723e50f50cbe486ce74992812e345aee3cc4
Sara Romão <sara-romao@live.com>
2022-05-21 15:38:09 -0300

    header estilo

--- sara-romao/src/components/Header.vue
+++ sara-romao/src/components/Header.vue
@@ -1,8 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/carrinho"><img :src="cartIcon" alt="Carrinho" /></router-link>
+  <header>
+    <nav>
+    <router-link class="logo" to="/"><img src="/assets/logo.svg" alt="logo style Store"/></router-link> 
+    <router-link class="cartIcon" to="/carrinho"><img src="/assets/cart.svg"  alt="Carrinho" /> <p class="inCart" v-if="cart"></p></router-link>
   </nav>
+  </header>
+  
 </template>
 
 <script>
@@ -11,7 +14,8 @@ export default {
 
   data(){
     return{
-       cartIcon:"/assets/cart.svg"
+      
+       
     }
   }
  
@@ -19,4 +23,73 @@ export default {
 </script>
 
 <style scoped>
+  header{
+    max-width: 100%;
+    box-shadow: 0px 4px 4px #F9F9F9;
+   
+  }
+
+  nav{
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 5px 20px 0px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+  }
+
+  @media screen and (max-width:425px){
+     nav{
+      padding: 5px 10px 0px;
+    }
+    
+  }
+
+  .cartIcon{
+    display: flex;
+    transform: scale(0.9);
+    text-decoration: none;
+    position: relative;
+    transition: 0.3s;
+    width: 50px;
+
+  }
+
+  .logo{
+    animation: animeZoom 0.8s ease-in-out forwards;
+    
+  }
+
+  @keyframes animeZoom {
+    from{
+      opacity: 0;
+      transform: scale(0.5);
+    }
+    to{
+      opacity: 1;
+      transform: scale(1);
+    }
+    
+  }
+
+  .cartIcon:hover{
+    transform: scale(1);
+
+  }
+
+  .inCart{
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    background: #aff8ce;
+    padding: 5px;
+    border-radius: 50%;
+    position: absolute;
+    right: 0;
+    top: -10px;
+  }
+
+  
+
 </style>
