bbb5eb986512fccc206e3e8f715c638128e88175
Sara Romão <sara-romao@live.com>
2022-05-23 18:03:48 -0300

    config vuex e store

---
+++ sara-romao/src/store/store.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+export const useStore = createStore({
+    state: {
+      count: 0
+    },
+    mutations: {
+      increment (state) {
+        state.count++
+      }
+    },
+    actions: {
+      increment (context) {
+        context.commit('increment')
+      }
+    }
+  })
