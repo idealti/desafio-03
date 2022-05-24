7dfc7339b402ae8a0c0a95bee108e7b09b04d42c
DanielValeriote <danielvaleriote@outlook.com>
2022-05-24 12:57:48 -0300

    create SearchInput component

---
+++ daniel-valeriote/src/components/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="input-container">
+		<input 
+		id="searchInput" 
+		type="text" 
+		:value="modelValue" 
+		@input="$emit('update:modelValue', $event.target.value)"
+		placeholder="Digite o que você procura..."
+		>	
+	</div>
+</template>
+<script setup>
+	defineProps(['modelValue']);
+	defineEmits(['update:modelValue']);
+</script>
+<style scoped>
+.input-container {
+	display: flex;
+	align-content: center;
+	justify-content: center;
+	padding-top: 25px;
+}
+input {
+	padding: 10px;
+	border-radius: 10px;
+	outline: 0;
+	border: 1px solid var(--main-black);
+	min-width: 200px;
+	width: 70%;
+	margin: 0 auto;
+}
+</style>
