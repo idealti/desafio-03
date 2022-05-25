413e280fd7abd0df726a9b6db3ffc91a9a1fdb70
Carlos Eduardo <cadu4duarte@gmail.com>
2022-05-25 13:26:14 -0300

    chore: add new route

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,7 +1,9 @@
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
+import Cart from "../pages/Cart.vue";
 
 export const routes = [
   { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "About", component: About }
+  { path: "/about", name: "About", component: About },
+  { path: "/cart", name: "Cart", component: Cart }
 ]
