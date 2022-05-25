2c62fd209170c237e2a9e017fbe6f8d443c92124
Carlos Eduardo <cadu4duarte@gmail.com>
2022-05-25 13:18:43 -0300

    chore: add routes

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+
+</script>
+
 <template>
+  <Header/>
   <router-view></router-view>
+  <Footer/>
 </template>
