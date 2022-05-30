d419b55f2103b5290a89a43367c61c6ca9f153ca
Carlos Eduardo <cadu4duarte@gmail.com>
2022-05-30 15:09:17 -0300

    chore: add about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,12 +4,50 @@
 
 <template>
   <div class="about">
-    <button>
-      <router-link to="/">Home</router-link>
-    </button>
+    <h1>Sobre Vuecommerce</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    
+    <div class="aboutBox">
+      <h2>developed with:</h2>
+      <img src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white"/>
+
+      <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white"/>
+
+      <img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"/>
+
+      <img src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D"/>
+
+    </div>
   </div>
 </template>
 
 <style>
 
+  .about {
+    width: 60%;
+    height: 35rem;
+    text-align: center;
+    padding: 20px;
+    margin: 0 auto;
+    
+  }
+
+  .about h1 {
+    padding: 10px;
+  }
+
+  .aboutBox {
+    margin-top: 150px;
+    padding: 10px;
+  }
+
+  h2 {
+    padding: 15px;
+  }
+
+  .aboutBox img {
+    margin: 10px;
+    border: none;
+  }
 </style>
